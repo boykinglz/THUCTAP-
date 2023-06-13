cec1dea098212c074a642d5bee3de299bba2e0c1
thanhdat011 <101719088+thanhdat011@users.noreply.github.com>
2023-06-13 18:03:27 +0700

    leave

--- Leave Manager/extra-addons/my_leave_manager/models/hr_holiday.py
+++ Leave Manager/extra-addons/my_leave_manager/models/hr_holiday.py
@@ -4,6 +4,6 @@ from odoo import fields, models
 class CustomHrHolidays(models.Model):
     _inherit = 'hr.leave'
 
-    def check_inheritance(self):
-        for test in self._inherit:
-            print("Test something:" +test)
+    @api.multi
+    def get_state_display(self):
+        return dict(self._fields['state'].selection).get(self.state)
