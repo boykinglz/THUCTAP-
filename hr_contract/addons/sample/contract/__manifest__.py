f7a3d5dcd23fbf3c43377815f5d9595520a71252
Dombletore <105104307+Dombletore@users.noreply.github.com>
2023-06-16 16:32:23 +0700

    Add files via upload

---
+++ hr_contract/addons/sample/contract/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Contract Management',
+    'depends': ['hr_contract'],
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/hr_contract_views.xml',
+    ],
+    
+}
