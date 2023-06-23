fec60a2c1dbf82945375984ddd10b9fa40e51680
DoTanPhat1 <phat31102001@gmail.com>
2023-06-23 11:25:00 +0700

    new

--- hr_attendance_tp/addons/sample/attendance/__manifest__.py
+++ hr_attendance_tp/addons/sample/attendance/__manifest__.py
@@ -2,8 +2,8 @@
     'name': 'Attendance neww',
     'depends': ['hr_attendance'],
     'data': [
-       # 'security/ir.model.access.csv',
         'views/hr_attendance_view.xml',
-        
+        'views/hr_report.xml' ,
+        'views/hr_report_templates.xml'
     ],
 }
